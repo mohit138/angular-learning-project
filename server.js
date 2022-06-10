8e98607dc04b771767fabe1e0151e340c7d78ac7
Mohit Gidwani <mohit22gidwani@gmail.com>
2022-06-10 19:22:46 +0530

    server.js minor change, adding base, for heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('./dist/angular-learning-project'));
 console.log("running server file");
 
 app.get('*', (req, res) => {
-    res.sendFile(`./dist/angular-learning-project/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname+`./dist/angular-learning-project/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port
