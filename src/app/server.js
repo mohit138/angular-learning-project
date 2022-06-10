96ec87d107e504610393e308d45cf565a3b14c2c
Mohit Gidwani <mohit22gidwani@gmail.com>
2022-06-10 18:02:43 +0530

    server.js update for heroku,9

---
+++ src/app/server.js
@@ -0,0 +1,21 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('../../dist/angular-learning-project'));
+
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/angular-learning-project/'}),
+// );
+
+console.log("running server file");
+
+app.get('*', (req, res) => {
+    res.sendFile(`../../dist/angular-learning-project/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
