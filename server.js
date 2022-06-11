ca9b9ad07d127907fb03520f3083ef7bec979d08
Mohit Gidwani <mohit22gidwani@gmail.com>
2022-06-11 21:26:57 +0530

    server.js updated, for router redirec issue

--- server.js
+++ server.js
@@ -14,8 +14,11 @@ app.use(express.static('./dist/angular-learning-project'));
 console.log("running server file");
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname+`./dist/angular-learning-project/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname+`/dist/angular-learning-project/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
+
+
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
